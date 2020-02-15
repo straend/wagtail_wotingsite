230d835f1ec054a9b777469e67e288c958cca79c
Tomas Strand <tomas@fik1.net>
2020-02-15 14:36:35 +0200

    Update requirements, add postgres pw

--- votingsite/votingsite/settings/production.py
+++ votingsite/votingsite/settings/production.py
@@ -24,6 +24,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
