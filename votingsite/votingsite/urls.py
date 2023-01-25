00f9d60749c16a5ab83b1b8783bcc43200da2d9d
Tomas Strand <tomas@fik1.net>
2023-01-25 22:15:14 +0200

    Upgrade packages

--- votingsite/votingsite/urls.py
+++ votingsite/votingsite/urls.py
@@ -1,12 +1,12 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-from django.urls import path
+from django.urls import path, re_path as url
 
 urlpatterns = [
     url(r'^django-admin/', admin.site.urls),
